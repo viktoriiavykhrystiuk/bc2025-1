9417b861f8089ba3bb4c75373b943e3ceac7a4d2
viktoriiavykhrystiuk <viktoriiavykhrystiuk@gmail.com>
2025-09-14 19:47:14 +0300

    app.js

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-console.log("Hello, world!");
-=======
 const githubUsername = "viktoriiavykhrystiuk"; 
 console.log(`Hello, ${githubUsername}!`);
->>>>>>> db1a111 (Оновила програму: вивід GitHub username)
+
